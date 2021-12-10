0fa8f33e88ad501daaf7a8d55dcd5540a89bf900
chenguzhen87 <546369005@qq.com>
2021-12-10 11:55:41 +0800

    feat: 首页功能

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -29,9 +29,9 @@ export default defineComponent({
               <CPage v-slots={slots} style="background:var(--van-gray-1)">
                   <NavBar  v-slots={navbarslots}></NavBar>
                   <VideoItem class="mb-2" />
-                  <AudioItem />
+                  <AudioItem class="mb-2" />
                   <VideoItem class="mb-2"/>
-                  <ArticleItem />
+                  <ArticleItem class="mb-2" />
                   <VideoItem class="mb-2"/>
               </CPage>
        )
@@ -97,7 +97,7 @@ const  AudioItem = (props: AnyObject)=>{
    return (
        <div class={styles.articleItemBox}>
           <div class="text-truncate">这里是音频标题这里是音频标题这里</div>
-          <div class="flex">
+          <div class="flex mt-3">
             <div class={styles.articleCover}>
                   <Image radius="6" width="100%" height="inherit" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg">
                   </Image>
